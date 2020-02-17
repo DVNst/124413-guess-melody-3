d6c5710ef8033081f4cdc952ed7a899cd73c18a2
DVNst <dvn-studio@mail.ru>
2020-02-18 00:48:20 +0300

    correct html

--- src/components/welcome-screen/welcome-screen.jsx
+++ src/components/welcome-screen/welcome-screen.jsx
@@ -15,10 +15,11 @@ const WelcomeScreen = (props) => {
       >
         <span className="visually-hidden">Начать игру</span>
       </button>
+      <h2 className="welcome__rules-title">Правила игры</h2>
       <p className="welcome__text">Правила просты:</p>
       <ul className="welcome__rules-list">
-        <li>Нужно ответить на все вопросы</li>
-        <li>Можно допустить {errorCount} ошибки</li>
+        <li>Нужно ответить на все вопросы.</li>
+        <li>Можно допустить {errorCount} ошибки.</li>
       </ul>
       <p className="welcome__text">Удачи!</p>
     </section>
