1c96962c9792e8ffa5860e66c9337875fffb20fa
DVNst <dvn-studio@mail.ru>
2020-02-18 23:36:42 +0300

    add test for artist-question-screen

--- src/components/artist-question-screen/artist-question-screen.jsx
+++ src/components/artist-question-screen/artist-question-screen.jsx
@@ -1,56 +1,67 @@
-import React from "react";
+import React, {PureComponent} from "react";
 import PropTypes from "prop-types";
 import {GameType} from "../../const.js";
 
-const ArtistQuestionScreen = (props) => {
-  const {question} = props;
+class ArtistQuestionScreen extends PureComponent {
+  constructor(props) {
+    super(props);
+  }
 
-  return (
-    <section className="game game--artist">
-      <header className="game__header">
-        <a className="game__back" href="#">
-          <span className="visually-hidden">Сыграть ещё раз</span>
-          <img className="game__logo" src="img/melody-logo-ginger.png" alt="Угадай мелодию" />
-        </a>
+  render() {
+    const {question, onAnswer} = this.props;
 
-        <svg xmlns="http://www.w3.org/2000/svg" className="timer" viewBox="0 0 780 780">
-          <circle className="timer__line" cx="390" cy="390" r="370"
-            style={{filter: `url(#blur)`, transform: `rotate(-90deg) scaleY(-1)`, transformOrigin: `center`}}/>
-        </svg>
+    return (
+      <section className="game game--artist">
+        <header className="game__header">
+          <a className="game__back" href="#">
+            <span className="visually-hidden">Сыграть ещё раз</span>
+            <img className="game__logo" src="img/melody-logo-ginger.png" alt="Угадай мелодию" />
+          </a>
 
-        <div className="game__mistakes">
-          <div className="wrong"></div>
-          <div className="wrong"></div>
-          <div className="wrong"></div>
-        </div>
-      </header>
+          <svg xmlns="http://www.w3.org/2000/svg" className="timer" viewBox="0 0 780 780">
+            <circle className="timer__line" cx="390" cy="390" r="370"
+              style={{filter: `url(#blur)`, transform: `rotate(-90deg) scaleY(-1)`, transformOrigin: `center`}}/>
+          </svg>
 
-      <section className="game__screen">
-        <h2 className="game__title">Кто исполняет эту песню?</h2>
-        <div className="game__track">
-          <div className="track">
-            <button className="track__button track__button--play" type="button"></button>
-            <div className="track__status">
-              <audio scr={question.track.src}></audio>
+          <div className="game__mistakes">
+            <div className="wrong"></div>
+            <div className="wrong"></div>
+            <div className="wrong"></div>
+          </div>
+        </header>
+
+        <section className="game__screen">
+          <h2 className="game__title">Кто исполняет эту песню?</h2>
+          <div className="game__track">
+            <div className="track">
+              <button className="track__button track__button--play" type="button"></button>
+              <div className="track__status">
+                <audio scr={question.track.src}></audio>
+              </div>
             </div>
           </div>
-        </div>
 
-        <form className="game__artist">
-          {question.answers.map((answer, index) => (
-            <div className="artist" key={answer.artist + index}>
-              <input className="artist__input visually-hidden" type="radio" name="answer" value={`artist-` + (index + 1)} id={`answer-` + (index + 1)} />
-              <label className="artist__name" htmlFor={`answer-` + (index + 1)}>
-                <img className="artist__picture" src={answer.picture} alt={answer.artist} />
-                {answer.artist}
-              </label>
-            </div>
-          ))}
-        </form>
+          <form className="game__artist">
+            {question.answers.map((answer, i) => (
+              <div className="artist" key={`${i}-${answer.artist}`}>
+                <input className="artist__input visually-hidden" type="radio" name="answer" value={`artist-${i}`} id={`answer-${i}`}
+                  onChange={(evt) => {
+                    evt.preventDefault();
+                    onAnswer(question, answer);
+                  }}
+                />
+                <label className="artist__name" htmlFor={`answer-${i}`}>
+                  <img className="artist__picture" src={answer.picture} alt={answer.artist} />
+                  {answer.artist}
+                </label>
+              </div>
+            ))}
+          </form>
+        </section>
       </section>
-    </section>
-  );
-};
+    );
+  }
+}
 
 ArtistQuestionScreen.propTypes = {
   question: PropTypes.shape({
@@ -63,7 +74,8 @@ ArtistQuestionScreen.propTypes = {
       picture: PropTypes.string.isRequired,
       artist: PropTypes.string.isRequired,
     })).isRequired,
-  })
+  }),
+  onAnswer: PropTypes.func.isRequired,
 };
 
 export default ArtistQuestionScreen;
---
+++ src/components/artist-question-screen/artist-question-screen.test.js
@@ -0,0 +1,34 @@
+import React from "react";
+import renderer from "react-test-renderer";
+import ArtistQuestionScreen from "./artist-question-screen.jsx";
+
+const question = {
+  type: `artist`,
+  track: {
+    artist: `artist1`,
+    src: `https://upload.wikimedia.org/wikipedia/commons/4/4e/BWV_543-fugue.ogg`,
+  },
+  answers: [
+    {
+      picture: `https://api.adorable.io/avatars/200/0`,
+      artist: `artist1`,
+    }, {
+      picture: `https://api.adorable.io/avatars/200/1`,
+      artist: `artist2`,
+    }, {
+      picture: `https://api.adorable.io/avatars/200/2`,
+      artist: `artist3`,
+    }
+  ],
+};
+
+it(`ArtistQuestionScreen is rendered correctly`, () => {
+  const tree = renderer.create(
+      <ArtistQuestionScreen
+        question={question}
+        onAnswer={() => {}}
+      />
+  ).toJSON();
+
+  expect(tree).toMatchSnapshot();
+});
